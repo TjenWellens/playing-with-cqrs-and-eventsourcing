6d068eca68751c3ecc88278da8bf55996b5431ce
Tjen Wellens <tjen.wellens@gmail.com>
2019-03-14 17:23:42 +0100

    log time

--- src/main/java/com/example/eventssplitjoin/EventLogger.java
+++ src/main/java/com/example/eventssplitjoin/EventLogger.java
@@ -4,10 +4,16 @@ import com.example.eventssplitjoin.events.Event;
 import com.google.common.eventbus.Subscribe;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 class EventLogger {
     @Subscribe
     public void handle(Event e) {
-        System.out.println(e.getClass().getSimpleName() + " - "+ e.toString());
+        final String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+        final String tag = e.getClass().getSimpleName();
+        final String message = e.toString();
+        System.out.println(date + "  " + tag + " --- " + message);
     }
 }
