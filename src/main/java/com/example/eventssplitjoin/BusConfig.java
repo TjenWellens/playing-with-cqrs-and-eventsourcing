44cee30e667719f8cd5521d4517d4d465b4502d6
Tjen Wellens <tjen.wellens@gmail.com>
2019-03-14 17:17:33 +0100

    async

--- src/main/java/com/example/eventssplitjoin/BusConfig.java
+++ src/main/java/com/example/eventssplitjoin/BusConfig.java
@@ -1,13 +1,16 @@
 package com.example.eventssplitjoin;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class BusConfig {
     @Bean
     EventBus eventBus() {
-        return new EventBus();
+        return new AsyncEventBus(Executors.newCachedThreadPool());
     }
 }
