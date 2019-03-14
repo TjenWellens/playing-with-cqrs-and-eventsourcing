ac76bbe6860ccf8992ee2410cafd08ba7e204e3e
Tjen Wellens <tjen.wellens@gmail.com>
2019-03-14 17:14:18 +0100

    Revert: sleep 0-5s when fetching images

--- src/main/java/com/example/eventssplitjoin/handlers/ImageCompletedPublisher.java
+++ src/main/java/com/example/eventssplitjoin/handlers/ImageCompletedPublisher.java
@@ -1,6 +1,5 @@
 package com.example.eventssplitjoin.handlers;
 
-import com.example.eventssplitjoin.domain.ImageType;
 import com.example.eventssplitjoin.events.ImageCompleted;
 import com.example.eventssplitjoin.events.IncompleteImageDiscovered;
 import com.google.common.eventbus.EventBus;
@@ -8,19 +7,23 @@ import com.google.common.eventbus.Subscribe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Set;
-
 @Component
 @RequiredArgsConstructor
 class ImageCompletedPublisher {
     private final EventBus bus;
 
-    private HashMap<String, Set<ImageType>> maneuverId_imageTypes = new HashMap<>();
-
     @Subscribe
     public void handle(IncompleteImageDiscovered e) {
-        // todo: fetch image, store image, store url
+        fetchImages();
+
         bus.post(new ImageCompleted(e.getProjectId(), e.getLeadId(), e.getManeuverId(), e.getImageType()));
+    }
+
+    private void fetchImages() {
+        // todo: fetch image, store image, store url
+        try {
+            Thread.sleep((long) (Math.random() * 5000));
+        } catch (InterruptedException e1) {
+        }
     }
 }
